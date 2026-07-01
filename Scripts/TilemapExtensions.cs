a69720ff9450bf6df4ced5ec81edd834fe185a4b
NoixDeXydre <noixdexydre@mailfence.com>
2026-07-01 23:57:08 +0200

    Add TilemapExtensions

---
+++ Scripts/TilemapExtensions.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Codomaster.Extensions
+{
+    public static class TilemapExtensions
+    {
+        /// <summary>
+        /// Copy tiles to another tilemap.
+        /// </summary>
+        /// <param name="sourceTilemap">Where to copy tiles.</param>
+        /// <param name="targetTilemap">Where to paste tiles.</param>
+        /// <returns>The target tilemap with copied tiles.</returns>
+        public static Tilemap CopyTilesToTilemap(this Tilemap sourceTilemap, Tilemap targetTilemap)
+        {
+            foreach (var position in sourceTilemap.cellBounds.allPositionsWithin)
+            {
+                if (sourceTilemap.HasTile(position))
+                    targetTilemap.SetTile(position, sourceTilemap.GetTile(position));
+            }
+
+            return targetTilemap;
+        }
+
+        /// <summary>
+        /// Flips the tilemap to the chosen direction..
+        /// <br></br>
+        /// Be careful with your properties as it changes 
+        /// the <see cref="Tilemap.Orientation.Custom"/> value.
+        /// </summary>
+        /// <param name="tilemap">The tilemap to flip.</param>
+        /// <param name="flipValue">Value used to rotate the tilemap.</param>
+        public static void CustomFlip(this Tilemap tilemap, Vector3 flipValue)
+        {
+            tilemap.orientation = Tilemap.Orientation.Custom;
+            tilemap.orientationMatrix = Matrix4x4.Scale(flipValue);
+        }
+
+        /// <summary>
+        /// Flips the tilemap horizontally.
+        /// </summary>
+        /// <param name="tilemap">The tilemap to flip.</param>
+        public static void FlipX(this Tilemap tilemap)
+        {
+            tilemap.CustomFlip(new Vector3(-1.0f, 1.0f, 1.0f));
+        }
+
+        /// <summary>
+        /// Flips the tilemap vertically.
+        /// </summary>
+        /// <param name="tilemap">The tilemap to flip.</param>
+        public static void FlipY(this Tilemap tilemap)
+        {
+            tilemap.CustomFlip(new Vector3(1.0f, -1.0f, 1.0f));
+        }
+    }
+}
